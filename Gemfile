117dee66501d2a98ac5abbf1fe69d894bc52c48a
kouki <miki.0418.kouki@gmail.com>
2018-06-06 08:42:47 +0900

    削除機能追加

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'jquery-rails'
 gem 'kaminari'
 gem 'ransack'
 gem 'devise'
+gem 'font-awesome-rails'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -14,6 +14,29 @@ class NotesController < ApplicationController
     end
   end
 
+  def edit
+    @note = Note.find(params[:id])
+    @notes =  Note.new_order
+    render action: :index
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    if @note.update(note_params)
+      redirect_to notes_path, notice: "メモを更新しました"
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+    redirect_to notes_path, notice: "メモを削除しました"
+
+  end
+
+
   private
 
   def note_params
